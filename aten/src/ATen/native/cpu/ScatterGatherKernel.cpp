habilitacionescarn3/pytorch

commit 28fadfc4eb7f7cff0a9fca69e30f50f1d7026a34

Reduce overheads on several CPU kernels by avoiding restrides. (#36875)
Summary:
Calling `t.as_strided(..., ...)` must make a new `TensorImpl` to back the new tensor, which takes 300-400 ns. Reduction, scatter/gather, and comparison kernels currently restride inputs and outputs in order to handle `dim` inside the function passed to TensorIterator. Because these Tensors are created solely for consumption by the iterator a full restride and metadata copy is surplus to requirements. Moreover, shapes are already checked by these kernels prior to calling `add_input` and `add_output`, so shape inference and broadcasting are also unnecessary.

This PR adds a `TensorIterator::declare_static_shape(...)` method, which allows certain kernels to use a much more constrained and efficient shape path. This results in a 900-1200 ns speedup for `gather / scatter / scatter_add / cumsum / cumprod` and a 250-500 ns speedup for elementwise `min` and `max`.

Measurements were taken with [this python script](https://gist.github.com/robieta/51ac5db2f9c7e812d5ff264403ce4f92), which is driven by [this bash script](https://gist.github.com/robieta/1420e917cf38885de3093f8c3a7bd437). The general procedure for mitigating environmental skew is to repeatedly switch between an environment which is built with master and one which is built with this branch while running the python script. Within the python measurement script the following was used to reduce variation:
* Set number of threads to 1
* Aggressively and randomly interleave task measurements to limit correlation between tasks and system state based on when they were run or what task preceded the current one.
* Warmup period, dropping the first three passes through all of the tasks.
Two independent end-to-end runs are included since there is some variation even with the above measures. Overall measurement error seems to be about +/- 100 ns.

aten/src/ATen/native/cpu/ScatterGatherKernel.cpp
@@ -134,30 +134,13 @@ struct cpu_scatter_gather_base_kernel {
       gather_shape_check(self, dim, index);
     }
 
-    auto index_sizes = ensure_nonempty_vec(index.sizes().vec());
-    auto index_strides = ensure_nonempty_vec(index.strides().vec());
-
-    // `dim` is traversed in the kernel,
-    // that is why index.stride(dim) = 0 and index.size(dim) = 1.
-    // Also, index.size(dim) = 1 makes sure that TensorIterator.DimCounter
-    // has the following form : (i_1,..., i_{dim-1}, 0, i_{dim+1},...,i_n).
-    index_sizes[dim] = 1;
-    index_strides[dim] = 0;
-
-    // set self.shape = src.shape = index.shape,
-    // this defines the number of elements to iterate over,
-    // and set self.stride(dim) = src.stride(dim) = 0,
-    // because `dim` is traversed in the kernel.
-    auto self_restrided = restride_dim(self, dim, index_sizes);
-    auto index_restrided = index.as_strided(index_sizes, index_strides);
-    auto src_restrided = restride_dim(src, dim, index_sizes);
-
     auto iter = TensorIterator();
     iter.dont_compute_common_dtype();
     iter.dont_resize_outputs();
-    iter.add_output(self_restrided);
-    iter.add_input(src_restrided, src.device(), src.scalar_type());
-    iter.add_input(index_restrided);
+    iter.declare_static_shape(index.sizes(), /*squash_dim=*/dim);
+    iter.add_output(self);
+    iter.add_input(src, src.device(), src.scalar_type());
+    iter.add_input(index);
     iter.build();
 
     auto self_dim_stride = ensure_nonempty_stride(self, dim);

aten/src/ATen/native/TensorIterator.cpp
@@ -768,6 +768,8 @@ void TensorIterator::check_mem_overlaps() {
 }
 
 void TensorIterator::compute_shape() {
+  if (static_shape_) return;
+
   all_ops_same_shape_ = true;
   bool has_scalars = false;
   bool has_tensors = false;
@@ -824,7 +826,7 @@ void TensorIterator::compute_shape() {
 void TensorIterator::compute_strides() {
   for (auto& op : operands_) {
     if (op.tensor.defined()) {
-      auto original_shape = op.tensor.sizes();
+      IntArrayRef original_shape = static_shape_ ? shape_ : op.tensor.sizes();
       auto original_stride = op.tensor.strides();
       auto element_size_in_bytes = op.tensor.element_size();
       auto offset = ndim() - original_shape.size();

aten/src/ATen/native/TensorIterator.h
@@ -357,6 +357,23 @@ struct CAFFE2_API TensorIterator {
     resize_outputs_ = false;
   }
 
+  void declare_static_shape(IntArrayRef shape) {
+    // WARNING:
+    //   This will bypass all shape checking in the TensorIterator. Kernels which call this method
+    //   are expected to check shapes before calling `add_input` or `add_output`.
+    TORCH_CHECK(!resize_outputs_, "dont_resize_outputs() must be called before declare_static_shape(...)")
+    shape_ = shape;
+    static_shape_ = true;
+  }
+
+  void declare_static_shape(IntArrayRef shape, const int64_t squash_dim){
+    declare_static_shape(shape);
+    if (!shape_.size()) return;
+    TORCH_CHECK(squash_dim >= 0 && squash_dim < shape_.size(),
+                "squash_dim ", squash_dim, " must be in [0, ", shape_.size(), ").");
+    shape_[squash_dim] = 1;
+  }
+
   void build();
 
 protected:
@@ -397,6 +414,7 @@ protected:
   bool all_ops_same_shape_ = false;
   bool requires_channels_last_output_ = false;
   bool requires_channels_last_3d_output_ = false;
+  bool static_shape_ = false;
 };
 /// A container-like struct that acts as if it contains splits of a
 /// TensorIterator that can use 32-bit indexing. Taken together the splits cover

aten/src/ATen/native/cpu/ReduceOpsKernel.cpp
@@ -36,17 +36,12 @@ static inline void cpu_cum_base_kernel(Tensor& result,
     return;
   }
 
-  auto self_sizes = ensure_nonempty_vec(self.sizes().vec());
-  self_sizes[dim] = 1;
-
-  auto result_restrided = restride_dim(result, dim, self_sizes);
-  auto self_restrided = restride_dim(self, dim, self_sizes);
-
   auto iter = TensorIterator();
   iter.dont_compute_common_dtype();
   iter.dont_resize_outputs();
-  iter.add_output(result_restrided);
-  iter.add_input(self_restrided);
+  iter.declare_static_shape(self.sizes(), /*squash_dim=*/dim);
+  iter.add_output(result);
+  iter.add_input(self);
   iter.build();
 
   auto result_dim_stride = ensure_nonempty_stride(result, dim);

aten/src/ATen/native/cpu/TensorCompareKernel.cpp
@@ -41,15 +41,15 @@ static inline void compare_base_kernel(Tensor& result, Tensor& indice,
     indice.set_(indices_view);
   }
 
-  Tensor self_restrided = restride_dim(self, dim, self_sizes);
   auto self_dim_stride = ensure_nonempty_stride(self, dim);
 
   auto iter = TensorIterator();
   iter.dont_compute_common_dtype();
   iter.dont_resize_outputs();
+  iter.declare_static_shape(self.sizes(), /*squash_dim=*/dim);
   iter.add_output(result);
   iter.add_output(indice);
-  iter.add_input(self_restrided);
+  iter.add_input(self);
   iter.build();
 
   auto loop = [&](char** data, const int64_t* strides, int64_t n) {

commit 35be2d6c5630c145d3e9eceebd0774b5bc51a47d

extend gather shape check to handle incorrectly sized outputs (#37102)
Summary:
Fixes a safety issue (Nonsense values and segfaults) introduced by https://github.com/pytorch/pytorch/pull/36875 when in-place gather tries to use incorrect shapes.

Consider the following block of code:
```
k0 = 8
k1 = 8
m = 100

x = torch.rand((k0, k1))
ind = torch.randint(0, k0, (m, k1))
output = torch.empty((m, k1))

print(torch.gather(x, 0, ind, out=output))
print(torch.gather(x, 1, ind, out=output))
```

The first gather is legal, the second is not. (`ind` and `output` need to be transposed) Previously this was caught when the kernel tried to restride inputs for TensorIterator, but we can no longer rely on those checks and must test explicitly. If `m` is small the second gather returns gibberish; if it is large enough to push the read out of memory block the program segfaults.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/37102

Differential Revision: D21190580

Pulled By: robieta

fbshipit-source-id: 80175620d24ad3380d78995f7ec7dbf2627d2998

aten/src/ATen/native/cpu/ScatterGatherKernel.cpp
@@ -9,23 +9,37 @@ namespace {
 
 // Used for `gather`-like methods
 // Test:
-// 1. index.size(d) == self.size(d) for all d != dim
-void gather_shape_check(const Tensor& self, int64_t dim, const Tensor& index) {
+// 1. index.size(d) == src.size(d) for all d != dim
+// 2. index.size(d) == self.size(d) for all d
+void gather_shape_check(const Tensor& self, int64_t dim, const Tensor& index, const Tensor& src) {
   auto self_dims = ensure_nonempty_dim(self.dim());
+  auto src_dims = ensure_nonempty_dim(src.dim());
 
   TORCH_CHECK(self_dims == ensure_nonempty_dim(index.dim()),
     "Index tensor must have the same number of dimensions as input tensor"
   );
 
+  TORCH_CHECK(src_dims == ensure_nonempty_dim(index.dim()),
+    "Index tensor must have the same number of dimensions as output tensor"
+  );
+
   for (int64_t i = 0; i < self_dims; ++i) {
+    auto index_size = ensure_nonempty_size(index, i);
     if (i != dim) {
       TORCH_CHECK(
-        ensure_nonempty_size(index, i) == ensure_nonempty_size(self, i),
-        "Size does not match at dimension ", i,
-        " get ", ensure_nonempty_size(self, i),
+        index_size == ensure_nonempty_size(src, i),
+        "Output size does not match at dimension ", i,
+        " get ", ensure_nonempty_size(src, i),
         " vs ", ensure_nonempty_size(index, i)
       );
     }
+    TORCH_CHECK(
+      index_size == ensure_nonempty_size(self, i),
+      "Input size does not match at dimension ", i,
+      " get ", ensure_nonempty_size(self, i),
+      " vs ", ensure_nonempty_size(index, i)
+    );
+
   }
 }
 
@@ -131,7 +145,7 @@ struct cpu_scatter_gather_base_kernel {
       scatter_shape_check(self, dim, index, src);
     }
     else {
-      gather_shape_check(self, dim, index);
+      gather_shape_check(self, dim, index, src);
     }
 
     auto iter = TensorIterator();

test/test_torch.py
@@ -2534,6 +2534,9 @@ class _TestTorchMixin(object):
                     expected[i, j, k] = src[tuple(ii)]
         self.assertEqual(actual, expected, 0)
 
+        bad_src = torch.randn(*[i - 1 for i in idx_size])
+        self.assertRaises(RuntimeError, lambda: torch.gather(bad_src, dim, idx))
+
         if test_bounds:
             idx[0][0][0] = 23
             self.assertRaises(RuntimeError, lambda: torch.gather(src, dim, idx))