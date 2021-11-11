habilitacionescarn3/pytorch

commit 33790c4e0619b8db04b42c9bfa444ec3cbd75851

Implement histogramdd on CPU (#65318)
Summary:
Implements `torch.histogramdd` analogous to `numpy.histogramdd`.

Builds on https://github.com/pytorch/pytorch/pull/58780, generalizing the existing `torch.histogram` kernel to handle D-dimensional inputs.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/65318

Reviewed By: soulitzer

Differential Revision: D31654555

Pulled By: saketh-are

fbshipit-source-id: 14b781fac0fd3698b052dbd6f0fda46e50d4c5f1

aten/src/ATen/native/cpu/HistogramKernel.cpp
@@ -7,7 +7,10 @@
 
 #include <algorithm>
 #include <mutex>
+#include <numeric>
 #include <tuple>
+#include <functional>
+#include <ATen/TensorIndexing.h>
 
 namespace at { namespace native {
 
@@ -15,29 +18,34 @@ namespace {
 
 constexpr int64_t HISTOGRAM_GRAIN_SIZE = 200;
 
-/* The main algorithm. Maps the elements of input into the bins defined by bin_edges.
- * Expects that the elements of bin_edges are increasing; behavior is otherwise undefined.
+/* The main algorithm. Expects that the input tensor has shape (N, D).
+ * Expects that bin_edges contains D one-dimensional tensors, each specifying
+ * an increasing sequences of bin edges.
  *
- * Accepts a template argument of type BIN_SELECTION_ALGORITHM specifying how the
- * elements of input should be mapped into the bins:
+ * Interprets the input as N different D-dimensional coordinates and maps them
+ * into the D-dimensional bins defined by bin_edges, accumulating a D-dimensional
+ * histogram in the hist tensor.
  *
- *     - LINEAR_INTERPOLATION: bin_edges must contain a linear progression.
- *       Elements of input are mapped to bins by computing
+ * Accepts a template argument of type BIN_SELECTION_ALGORITHM specifying how
+ * the scalars in each dimension should be mapped into the dimension's bins:
+ *
+ *     - LINEAR_INTERPOLATION: each bin edge sequence must form a linear progression.
+ *       Scalars are mapped to bins by computing
  *           (element - leftmost_edge)/(rightmost_edge - leftmost_edge) * bin_ct
  *       and truncating the result to an integer.
  *
- *       Results may not be perfectly consistent with the boundaries specified in bin_edges
- *       due to precision issues.
+ *       This is the fastest option, but its results may not be perfectly consistent
+ *       with the boundaries specified in bin_edges due to precision issues.
  *
  *       Used by torch.histc, which doesn't need consistency with bin_edges as it does not
  *       return bin_edges. Additionally, this implementation is identical to the legacy histc
  *       implementation, which was replaced when histogram was implemented.
  *
- *     - LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH: Also expects that bin_edges contains a
- *       linear progression. For each element, if 'pos' is the bin selected by the
+ *     - LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH: Also expects that each bin edge sequence
+ *       forms a linear progression. For each scalar, if 'pos' is the bin selected by the
  *       LINEAR_INTERPOLATION approach, this approach inspects the boundaries in bin_edges to
- *       place the element into pos - 1, pos, or pos + 1. The "local search" over neighboring
- *       bins allows for correction of misclassifications due to precision issues (an element
+ *       place the scalar into pos - 1, pos, or pos + 1. The "local search" over neighboring
+ *       bins allows for correction of misclassifications due to precision issues (a scalar
  *       very close to a bin_edge may be misclassified by LINEAR_INTERPOLATION).
  *
  *       Should produce the same output as the general case BINARY_SEARCH, but run about
@@ -54,8 +62,6 @@ constexpr int64_t HISTOGRAM_GRAIN_SIZE = 200;
  *
  * See discussion at https://github.com/pytorch/pytorch/pull/58780#discussion_r648604866
  * for further details on relative performance of the bin selection algorithms.
- *
- * Accumulates the total weight in each bin into the hist tensor.
  */
 enum BIN_SELECTION_ALGORITHM {
     LINEAR_INTERPOLATION,
@@ -63,17 +69,28 @@ enum BIN_SELECTION_ALGORITHM {
     BINARY_SEARCH,
 };
 template<typename input_t, BIN_SELECTION_ALGORITHM algorithm>
-void histogram_cpu_contiguous(Tensor& hist, const Tensor& bin_edges,
+void histogramdd_cpu_contiguous(Tensor& hist, const TensorList& bin_edges,
         const Tensor& input, const c10::optional<Tensor>& weight) {
-    TORCH_INTERNAL_ASSERT(hist.is_contiguous());
-    TORCH_INTERNAL_ASSERT(bin_edges.is_contiguous());
-    TORCH_INTERNAL_ASSERT(hist.numel() + 1 == bin_edges.numel());
-    TORCH_INTERNAL_ASSERT(input.dim() == 1);
-    TORCH_INTERNAL_ASSERT(!weight.has_value() || weight.value().dim() == 1);
+    TORCH_INTERNAL_ASSERT(input.dim() == 2);
 
-    const int64_t numel_in = input.numel();
+    const int64_t N = input.size(0);
+    if (weight.has_value()) {
+        TORCH_INTERNAL_ASSERT(weight.value().dim() == 1 && weight.value().numel() == N);
+    }
 
-    TensorAccessor<input_t, 1> accessor_in = input.accessor<input_t, 1>();
+    const int64_t D = input.size(1);
+    TORCH_INTERNAL_ASSERT(int64_t(bin_edges.size()) == D);
+    for (int64_t dim = 0; dim < D; dim++) {
+        TORCH_INTERNAL_ASSERT(bin_edges[dim].is_contiguous());
+        TORCH_INTERNAL_ASSERT(hist.size(dim) + 1 == bin_edges[dim].numel());
+    }
+
+    if (D == 0) {
+        // hist is an empty tensor in this case; nothing to do here
+        return;
+    }
+
+    TensorAccessor<input_t, 2> accessor_in = input.accessor<input_t, 2>();
 
     /* Constructs a c10::optional<TensorAccessor> containing an accessor iff
      * the optional weight tensor has a value.
@@ -82,73 +99,91 @@ void histogram_cpu_contiguous(Tensor& hist, const Tensor& bin_edges,
             ? c10::optional<TensorAccessor<input_t, 1>>(weight.value().accessor<input_t, 1>())
             : c10::optional<TensorAccessor<input_t, 1>>();
 
-    const int64_t numel_be = bin_edges.numel();
-    const input_t *data_be = bin_edges.data_ptr<input_t>();
+    std::vector<input_t*> bin_seq(D);
+    std::vector<int64_t> num_bin_edges(D);
+    std::vector<input_t> leftmost_edge(D), rightmost_edge(D);
 
-    const input_t leftmost_bin_edge = data_be[0];
-    const input_t rightmost_bin_edge = data_be[numel_be - 1];
+    for (int64_t dim = 0; dim < D; dim++) {
+        bin_seq[dim] = bin_edges[dim].data_ptr<input_t>();
+        num_bin_edges[dim] = bin_edges[dim].numel();
+        leftmost_edge[dim] = bin_seq[dim][0];
+        rightmost_edge[dim] = bin_seq[dim][num_bin_edges[dim] - 1];
+    }
 
-    input_t *data_out = hist.data_ptr<input_t>();
+    int64_t GRAIN_SIZE = std::max(int64_t(1), HISTOGRAM_GRAIN_SIZE / D);
 
     /* Parallelizes processing of input using at::parallel_for.
-     * Each thread accumulates a local result for some range of the input in data_out_local
-     * before locking data_out_mutex and adding its accumulated results to data_out.
+     * Each thread accumulates a local result for some range of the input in hist_local
+     * before locking hist_mutex and adding its accumulated results to the hist tensor.
      */
-    std::mutex data_out_mutex;
-    at::parallel_for(0, numel_in, HISTOGRAM_GRAIN_SIZE, [&](int64_t start, int64_t end) {
-        // Allocates a buffer for the thread's local results
-        std::vector<input_t> data_out_local(numel_be - 1, input_t(0));
+    std::mutex hist_mutex;
+    at::parallel_for(0, N, GRAIN_SIZE, [&](int64_t start, int64_t end) {
+        // Allocates a tensor for the thread's local results
+        Tensor hist_local = at::zeros(hist.sizes(), hist.dtype());
 
+        std::vector<at::indexing::TensorIndex> indices(D, 0);
         for (const auto i : c10::irange(start, end)) {
-            const input_t elt = accessor_in[i];
+            bool skip_elt = false;
 
-            // Skips elements which fall outside the specified bins
-            if (elt < leftmost_bin_edge || rightmost_bin_edge < elt) {
-                continue;
-            }
+            for (int64_t dim = 0; dim < D; dim++) {
+                const input_t elt = accessor_in[i][dim];
 
-            int64_t pos = -1;
-
-            if (algorithm == BINARY_SEARCH) {
-                // Handles the general case via binary search on the bin edges.
-                pos = std::upper_bound(data_be, data_be + numel_be, elt) - data_be - 1;
-            } else if (algorithm == LINEAR_INTERPOLATION
-                    || algorithm == LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH) {
-                /* When bin_edges is known to be a linear progression, maps elt to
-                 * the appropriate bin via simple division.
-                 */
-                pos = static_cast<int64_t>((elt - leftmost_bin_edge)
-                        / (rightmost_bin_edge - leftmost_bin_edge)
-                        * (numel_be - 1));
-
-                /* Ensures consistency with bin_edges by checking the bins to the left and right
-                 * of the selected position. Necessary for cases in which an element very close
-                 * to a bin edge may be misclassified by simple division.
-                 */
-                if (algorithm == LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH) {
-                    int64_t pos_min = std::max(static_cast<int64_t>(0), pos - 1);
-                    int64_t pos_max = std::min(pos + 2, numel_be);
-                    pos = std::upper_bound(data_be + pos_min, data_be + pos_max, elt) - data_be - 1;
+                // Skips elements which fall outside the specified bins
+                if (elt < leftmost_edge[dim] || rightmost_edge[dim] < elt) {
+                    skip_elt = true;
+                    break;
                 }
-            } else {
-                TORCH_INTERNAL_ASSERT(false);
+
+                int64_t pos = -1;
+
+                if (algorithm == BINARY_SEARCH) {
+                    // Handles the general case via binary search on the bin edges.
+                    pos = std::upper_bound(bin_seq[dim], bin_seq[dim] + num_bin_edges[dim], elt)
+                            - bin_seq[dim] - 1;
+                } else if (algorithm == LINEAR_INTERPOLATION
+                        || algorithm == LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH) {
+                    /* When bin_edges is known to be a linear progression, maps elt to
+                     * the appropriate bin via simple division.
+                     */
+                    pos = static_cast<int64_t>((elt - leftmost_edge[dim])
+                            / (rightmost_edge[dim] - leftmost_edge[dim])
+                            * (num_bin_edges[dim] - 1));
+
+                    /* Ensures consistency with bin_edges by checking the bins to the left and right
+                     * of the selected position. Necessary for cases in which an element very close
+                     * to a bin edge may be misclassified by simple division.
+                     */
+                    if (algorithm == LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH) {
+                        int64_t pos_min = std::max(static_cast<int64_t>(0), pos - 1);
+                        int64_t pos_max = std::min(pos + 2, num_bin_edges[dim]);
+                        pos = std::upper_bound(bin_seq[dim] + pos_min, bin_seq[dim] + pos_max, elt)
+                                - bin_seq[dim] - 1;
+                    }
+                } else {
+                    TORCH_INTERNAL_ASSERT(false);
+                }
+
+                // Unlike other bins, the rightmost bin includes its right boundary
+                if (pos == (num_bin_edges[dim] - 1)) {
+                    pos -= 1;
+                }
+
+                indices[dim] = pos;
             }
 
-            // Unlike other bins, the rightmost bin includes its right boundary
-            if (pos == (numel_be - 1)) {
-                pos -= 1;
-            }
+            if (!skip_elt) {
+                // In the unweighted case, the default weight is 1
+                input_t wt = accessor_wt.has_value() ? accessor_wt.value()[i] : static_cast<input_t>(1);
 
-            // In the unweighted case, the default weight is 1
-            input_t wt = accessor_wt.has_value() ? accessor_wt.value()[i] : static_cast<input_t>(1);
-            data_out_local[pos] += wt;
+                input_t cur = hist_local.index(indices).item<input_t>();
+                hist_local.index_put_(indices, cur + wt);
+            }
         }
+
 
         // Locks and updates the common output
-        const std::lock_guard<std::mutex> lock(data_out_mutex);
-        for (int64_t i = 0; i < numel_be - 1; i++) {
-            data_out[i] += data_out_local[i];
-        }
+        const std::lock_guard<std::mutex> lock(hist_mutex);
+        hist.add_(hist_local);
     });
 }
 
@@ -156,64 +191,85 @@ void histogram_cpu_contiguous(Tensor& hist, const Tensor& bin_edges,
  * Initializes hist to 0, calls into the main algorithm, and normalizes output if necessary.
  */
 template<BIN_SELECTION_ALGORITHM bin_algorithm>
-void histogram_out_cpu_template(const Tensor& self, const c10::optional<Tensor>& weight, bool density,
-        Tensor& hist, const Tensor& bin_edges) {
+void histogramdd_out_cpu_template(const Tensor& self, const c10::optional<Tensor>& weight, bool density,
+        Tensor& hist, const TensorList& bin_edges) {
     hist.fill_(0);
 
-    const int64_t numel_in = self.numel();
-    const Tensor reshaped_input = self.reshape({numel_in});
+    const int64_t N = self.size(-1);
+    const int64_t M = std::accumulate(self.sizes().begin(), self.sizes().end() - 1,
+            (int64_t)1, std::multiplies<int64_t>());
+
+    const Tensor reshaped_input = self.reshape({M, N});
 
     const auto reshaped_weight = weight.has_value()
-            ? c10::optional<Tensor>(weight.value().reshape({numel_in}))
+            ? c10::optional<Tensor>(weight.value().reshape({M}))
             : c10::optional<Tensor>();
 
+    std::vector<Tensor> bin_edges_contig(bin_edges.size());
+    for (size_t dim = 0; dim < bin_edges_contig.size(); dim++) {
+        bin_edges_contig[dim] = bin_edges[dim].contiguous();
+    }
+
     AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "histogram_cpu", [&]() {
-        histogram_cpu_contiguous<scalar_t, bin_algorithm>(
-                hist, bin_edges.contiguous(), reshaped_input, reshaped_weight);
+        histogramdd_cpu_contiguous<scalar_t, bin_algorithm>(
+                hist, bin_edges_contig, reshaped_input, reshaped_weight);
     });
 
-    // Converts the bin totals to a probability density function
+    /* Divides each bin's value by the total count/weight in all bins,
+     * and by the bin's volume.
+     */
     if (density) {
-        auto bin_widths = bin_edges.diff();
-        auto hist_sum = hist.sum().item();
-        hist.div_(bin_widths);
+        const auto hist_sum = hist.sum().item();
         hist.div_(hist_sum);
+
+         /* For each dimension, divides each bin's value
+          * by the bin's length in that dimension.
+          */
+        for (int64_t dim = 0; dim < N; dim++) {
+            const auto bin_lengths = bin_edges[dim].diff();
+
+            // Used to reshape bin_lengths to align with the corresponding dimension of hist.
+            std::vector<int64_t> shape(N, 1);
+            shape[dim] = bin_lengths.numel();
+
+            hist.div_(bin_lengths.reshape(shape));
+        }
     }
 }
 
 /* The general implementation of the histogram kernel. Maps each element of the input tensor
  * to its corresponding bin by performing a binary search over the elements of bin_edges.
  *
- * Refer to histogram_out_cpu_template for more details.
+ * Refer to histogramdd_out_cpu_template for more details.
  */
-static void histogram_kernel_impl(const Tensor& self, const c10::optional<Tensor>& weight, bool density,
-        Tensor& hist, const Tensor& bin_edges) {
-    histogram_out_cpu_template<BINARY_SEARCH>(self, weight, density, hist, bin_edges);
+static void histogramdd_kernel_impl(const Tensor& self, const c10::optional<Tensor>& weight, bool density,
+        Tensor& hist, const TensorList& bin_edges) {
+    histogramdd_out_cpu_template<BINARY_SEARCH>(self, weight, density, hist, bin_edges);
 }
 
 /* A faster version of the histogram kernel for cases in which bin_edges are known
  * to form a linear progression.
  *
- * Refer to histogram_out_cpu_template for more details.
+ * Refer to histogramdd_out_cpu_template for more details.
  */
-static void histogram_linear_kernel_impl(const Tensor& self, const c10::optional<Tensor>& weight,
-        bool density, Tensor& hist, const Tensor& bin_edges, bool local_search) {
+static void histogramdd_linear_kernel_impl(const Tensor& self, const c10::optional<Tensor>& weight,
+        bool density, Tensor& hist, const TensorList& bin_edges, bool local_search) {
     if (local_search) {
-        // histogram codepath: both hist and bin_edges are eventually returned as output,
+        // histogramdd codepath: both hist and bin_edges are eventually returned as output,
         // so we'll keep them consistent
-        histogram_out_cpu_template<LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH>(
+        histogramdd_out_cpu_template<LINEAR_INTERPOLATION_WITH_LOCAL_SEARCH>(
               self, weight, density, hist, bin_edges);
     } else {
         // histc codepath: bin_edges are not returned to the caller
-        histogram_out_cpu_template<LINEAR_INTERPOLATION>(
+        histogramdd_out_cpu_template<LINEAR_INTERPOLATION>(
               self, weight, density, hist, bin_edges);
     }
 }
 
 } // namespace
 
-REGISTER_DISPATCH(histogram_stub, &histogram_kernel_impl);
+REGISTER_DISPATCH(histogramdd_stub, &histogramdd_kernel_impl);
 
-REGISTER_DISPATCH(histogram_linear_stub, &histogram_linear_kernel_impl);
+REGISTER_DISPATCH(histogramdd_linear_stub, &histogramdd_linear_kernel_impl);
 
 }} // namespace at::native

aten/src/ATen/core/aten_interned_strings.h
@@ -379,6 +379,7 @@ _(aten, heaviside) \
 _(aten, hinge_embedding_loss) \
 _(aten, histc) \
 _(aten, histogram) \
+_(aten, histogramdd) \
 _(aten, hspmm) \
 _(aten, hsplit) \
 _(aten, hstack) \

aten/src/ATen/native/Histogram.h
@@ -7,11 +7,10 @@
 
 namespace at { namespace native {
 
-using histogram_fn = void(*)(const Tensor&, const c10::optional<Tensor>&, bool, Tensor&, const Tensor&);
-using histogram_linear_fn = void(*)(const Tensor&, const c10::optional<Tensor>&, bool, Tensor&, const Tensor&, bool);
+using histogramdd_fn = void(*)(const Tensor&, const c10::optional<Tensor>&, bool, Tensor&, const TensorList&);
+using histogramdd_linear_fn = void(*)(const Tensor&, const c10::optional<Tensor>&, bool, Tensor&, const TensorList&, bool);
 
-DECLARE_DISPATCH(histogram_fn, histogram_stub);
-
-DECLARE_DISPATCH(histogram_linear_fn, histogram_linear_stub);
+DECLARE_DISPATCH(histogramdd_fn, histogramdd_stub);
+DECLARE_DISPATCH(histogramdd_linear_fn, histogramdd_linear_stub);
 
 }} // namespace at::native

aten/src/ATen/native/native_functions.yaml
@@ -7095,6 +7095,18 @@
   dispatch:
     CPU: histogram_cpu
 
+- func: _histogramdd_bin_edges(Tensor self, int[] bins, *, float[]? range=None, Tensor? weight=None, bool density=False) -> Tensor[]
+  dispatch:
+    CPU: histogramdd_bin_edges_cpu
+
+- func: _histogramdd_from_bin_cts(Tensor self, int[] bins, *, float[]? range=None, Tensor? weight=None, bool density=False) -> Tensor
+  dispatch:
+    CPU: histogramdd_cpu
+
+- func: _histogramdd_from_bin_tensors(Tensor self, Tensor[] bins, *, Tensor? weight=None, bool density=False) -> Tensor
+  dispatch:
+    CPU: histogramdd_cpu
+
 - func: fmod.Scalar_out(Tensor self, Scalar other, *, Tensor(a!) out) -> Tensor(a!)
   device_check: NoCheck   # TensorIterator
   dispatch:

test/test_fx.py
@@ -3188,6 +3188,7 @@ class TestOperatorSignatures(JitTestCase):
                            'expand_as',
                            'fill_',
                            'hstack',
+                           'histogramdd',
                            'igamma',
                            'igammac',
                            'linalg.multi_dot',

test/test_fx_experimental.py
@@ -1475,6 +1475,7 @@ class TestNormalizeOperators(JitTestCase):
             "mT",  # Implemented with a lambda
             "mH",  # Implemented with a lambda
             "gradient",
+            "histogramdd",
             "igamma",
             "igammac",
             "index_put",

commit 2848ac79e619409c44f264048a6b649d518dc996

Avoid index_put_ overhead in histogram kernel's inner loop (#67815)
Summary:
**TLDR**: Makes torch.histc run 400x faster on large inputs. Should fix [a broken test on internal CI](https://www.internalfb.com/intern/test/281475013640093/).

HistogramKernel presently calls torch.Tensor.index_put_ once for each element of its input tensor. Obtaining a data pointer and manipulating it directly avoids the considerable dispatch overhead from calling index_put_. Behavior is unchanged because the tensor being operated on is known to be contiguous and in CPU memory.

Fixes performance regression introduced in https://github.com/pytorch/pytorch/pull/65318.

Benchmark: time taken to compute histc on a tensor with 10,000,000 elements

1. Before https://github.com/pytorch/pytorch/pull/65318: **0.003s**
2. After https://github.com/pytorch/pytorch/pull/65318: **2.154s**
3. After this change: **0.005s**

Benchmark code:
```
import torch as t
from timeit import default_timer as timer

x = t.randperm(10000000, dtype=t.float32)

start = timer()
t.histc(x)
end = timer()
print(end - start)
```

Pull Request resolved: https://github.com/pytorch/pytorch/pull/67815

Reviewed By: anjali411

Differential Revision: D32357663

Pulled By: saketh-are

fbshipit-source-id: f8fa59173ea4772c8ad1332548ef4d9ea8f01178

aten/src/ATen/native/cpu/HistogramKernel.cpp
@@ -120,10 +120,15 @@ void histogramdd_cpu_contiguous(Tensor& hist, const TensorList& bin_edges,
     at::parallel_for(0, N, GRAIN_SIZE, [&](int64_t start, int64_t end) {
         // Allocates a tensor for the thread's local results
         Tensor hist_local = at::zeros(hist.sizes(), hist.dtype());
+        TORCH_INTERNAL_ASSERT(hist_local.is_contiguous());
 
-        std::vector<at::indexing::TensorIndex> indices(D, 0);
+        input_t *hist_local_data = hist_local.data_ptr<input_t>();
+
+        const auto hist_strides = hist_local.strides();
+
         for (const auto i : c10::irange(start, end)) {
             bool skip_elt = false;
+            int64_t hist_index = 0;
 
             for (int64_t dim = 0; dim < D; dim++) {
                 const input_t elt = accessor_in[i][dim];
@@ -168,15 +173,14 @@ void histogramdd_cpu_contiguous(Tensor& hist, const TensorList& bin_edges,
                     pos -= 1;
                 }
 
-                indices[dim] = pos;
+                hist_index += hist_strides[dim] * pos;
             }
 
             if (!skip_elt) {
                 // In the unweighted case, the default weight is 1
                 input_t wt = accessor_wt.has_value() ? accessor_wt.value()[i] : static_cast<input_t>(1);
 
-                input_t cur = hist_local.index(indices).item<input_t>();
-                hist_local.index_put_(indices, cur + wt);
+                hist_local_data[hist_index] += wt;
             }
         }
 