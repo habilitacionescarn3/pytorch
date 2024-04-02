habilitacionescarn3/pytorch

commit 35382f05735aab813fbf94535a71441da092ae6e

[dynamo, 3.12] Use CPython internal _PyOpcode_Caches instead of hardcoding (#122335)
Pull Request resolved: https://github.com/pytorch/pytorch/pull/122335
Approved by: https://github.com/jansel
ghstack dependencies: #122146

torch/csrc/dynamo/cpython_defs.h
@@ -27,3 +27,7 @@ void THP_PyThreadState_PopFrame(
     _PyInterpreterFrame* frame);
 
 #endif
+
+// pointers to _PyOpcode_Caches for C++
+extern const uint8_t* THP_PyOpcode_Caches;
+extern const int THP_PyOpcode_Caches_size;

torch/_C/_dynamo/eval_frame.pyi
@@ -22,3 +22,5 @@ class _ExtraState:
     def invalidate(self, cache_entry: _CacheEntry): ...
 
 def _debug_get_cache_entry_list(code: types.CodeType) -> List[_CacheEntry]: ...
+
+py_opcode_caches: List[int]

torch/_dynamo/bytecode_transformation.py
@@ -852,26 +852,11 @@ def fix_extended_args(instructions: List[Instruction]) -> int:
     return added
 
 
-# from https://github.com/python/cpython/blob/v3.11.1/Include/internal/pycore_opcode.h#L41
-# TODO use the actual object instead, can interface from eval_frame.c
-_PYOPCODE_CACHES = {
-    "BINARY_SUBSCR": 4,
-    "STORE_SUBSCR": 1,
-    "UNPACK_SEQUENCE": 1,
-    "STORE_ATTR": 4,
-    "LOAD_ATTR": 4,
-    "COMPARE_OP": 2,
-    "LOAD_GLOBAL": 5,
-    "BINARY_OP": 1,
-    "LOAD_METHOD": 10,
-    "PRECALL": 1,
-    "CALL": 4,
-}
-
-
 def instruction_size(inst) -> int:
+    import torch
+
     if sys.version_info >= (3, 11):
-        return 2 * (_PYOPCODE_CACHES.get(dis.opname[inst.opcode], 0) + 1)
+        return 2 * (torch._C._dynamo.eval_frame.py_opcode_caches[inst.opcode] + 1)
     return 2
 
 

torch/csrc/dynamo/cpython_defs.c
@@ -26,7 +26,7 @@
 
 #define Py_BUILD_CORE
 #include <internal/pycore_pystate.h>
-#define NEED_OPCODE_TABLES // To get _PyOpcode_Deopt
+#define NEED_OPCODE_TABLES // To get _PyOpcode_Deopt, _PyOpcode_Caches
 #include <internal/pycore_opcode.h>
 #undef NEED_OPCODE_TABLES
 #undef Py_BUILD_CORE
@@ -646,5 +646,17 @@ THP_PyThreadState_PopFrame(PyThreadState *tstate, _PyInterpreterFrame * frame)
     }
 }
 
+
+#endif
+
+#if IS_PYTHON_3_11_PLUS
+
+const uint8_t* THP_PyOpcode_Caches = _PyOpcode_Caches;
+const int THP_PyOpcode_Caches_size = sizeof(_PyOpcode_Caches) / sizeof(uint8_t);
+
+#else
+
+const uint8_t* THP_PyOpcode_Caches = NULL;
+const int THP_PyOpcode_Caches_size = 0;
 
 #endif

torch/csrc/dynamo/init.cpp
@@ -1,16 +1,35 @@
 #include <torch/csrc/dynamo/init.h>
 
+#include <pybind11/stl_bind.h>
 #include <torch/csrc/Exceptions.h>
 #include <torch/csrc/dynamo/cache_entry.h>
+#include <torch/csrc/dynamo/cpython_defs.h>
 #include <torch/csrc/dynamo/eval_frame.h>
 #include <torch/csrc/dynamo/extra_state.h>
 #include <torch/csrc/dynamo/guards.h>
 #include <torch/csrc/dynamo/python_compiled_autograd.h>
+#include <torch/csrc/utils/pybind.h>
+#include <torch/csrc/utils/python_compat.h>
 
 static struct PyModuleDef _module =
     {PyModuleDef_HEAD_INIT, "torch._C._dynamo", "", -1, nullptr};
 
+PYBIND11_MAKE_OPAQUE(std::vector<uint8_t>);
+
 namespace torch::dynamo {
+
+#if IS_PYTHON_3_11_PLUS
+
+std::vector<uint8_t> _PyOpcode_Caches_vec(
+    THP_PyOpcode_Caches,
+    THP_PyOpcode_Caches + THP_PyOpcode_Caches_size);
+
+#else
+
+std::vector<uint8_t> _PyOpcode_Caches_vec;
+
+#endif
+
 using torch::dynamo::autograd::torch_c_dynamo_compiled_autograd_init;
 
 void initDynamoBindings(PyObject* torch) {
@@ -47,6 +66,8 @@ void initDynamoBindings(PyObject* torch) {
       .def("invalidate", &ExtraState::invalidate);
 
   m.def("_debug_get_cache_entry_list", &_debug_get_cache_entry_list);
+  py::bind_vector<std::vector<uint8_t>>(m, "VectorUInt8");
+  m.attr("py_opcode_caches") = _PyOpcode_Caches_vec;
 }
 
 } // namespace torch::dynamo

commit 16f1f8f3412f3e1f7a5e637ea9f0ef30de7f71b7

[dynamo] fix broken 3.11+ windows build failure (#123104)
e.g. https://github.com/pytorch/pytorch/actions/runs/8478510063/job/23230951466#step:12:23296

Caused by https://github.com/pytorch/pytorch/pull/122335

Pull Request resolved: https://github.com/pytorch/pytorch/pull/123104
Approved by: https://github.com/atalman

torch/csrc/dynamo/cpython_defs.h
@@ -29,5 +29,14 @@ void THP_PyThreadState_PopFrame(
 #endif
 
 // pointers to _PyOpcode_Caches for C++
+#ifdef __cplusplus
+
+extern "C" const uint8_t* THP_PyOpcode_Caches;
+extern "C" const int THP_PyOpcode_Caches_size;
+
+#else
+
 extern const uint8_t* THP_PyOpcode_Caches;
 extern const int THP_PyOpcode_Caches_size;
+
+#endif